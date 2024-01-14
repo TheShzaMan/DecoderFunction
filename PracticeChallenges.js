156e2fe287403d3129f8d1b08daa4bb295563da5
Shea Raymond <shea.raymond01@gmail.com>
2024-01-14 17:43:59 -0600

    Adding function that filters out the zeros of an array and puts them all at the end of the array without changing the order of the rest of the array.

--- PracticeChallenges.js
+++ PracticeChallenges.js
@@ -14,5 +14,43 @@ function solution(number) {
 	}
 	return sumResult;
 }
+//solution(10);
 
-solution(10);
+//  A function that takes an array and moves all of the zeros to the end,
+//  preserving the order of the other elements, and returns the array.
+function moveZeros(arr) {
+	//const newArr = [];
+	const zeros = [];
+
+	const newArr = arr
+		.filter((x) => {
+			if (x !== 0) {
+				return true;
+			} else {
+				zeros.push(x);
+				return false;
+			}
+		})
+		.concat(zeros);
+	console.log(newArr);
+	// for (let i = 1; i < arr.length; i++) {
+
+	// }
+	// console.log(arr.length);
+	//const newArr = arr.map(x => {x === 0 ?  zerosArr.push(x) : })
+
+	// .map(element =>
+	//     arr.filter(element => (element === 0), zerosArr.push(element):(newArr.push(element));})
+	// })
+
+	// for (let i = 0; i < arr.length; i++) {
+	// 	if (arr[i] === 0) {
+	// 		zerosArr.push(arr[i]);
+	// 	} else {
+	// 		newArr.push(arr[i]);
+	// 	}
+	// }
+	// console.log(newArr.concat(zerosArr));
+	// return newArr;
+}
+moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]); // returns[false,1,1,2,1,3,"a",0,0]
